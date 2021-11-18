34a4c36de6a69d6d8c088cd38ee6a059852b9a0f
Hugh Goode <hjgoode3@gmail.com>
2021-11-17 20:04:03 -0500

    add ordinal encoding function

--- data_preprocessing_final.py
+++ data_preprocessing_final.py
@@ -83,3 +83,30 @@ def dummify_func(housing):
     housing_num_scaled = pd.DataFrame(housing_num_scaled, columns = housing_num.columns)
     full_dum_data = pd.concat([housing_num_PID, housing_num_scaled, cat_dum], axis = 1) #Concatenate dummified data and numeric data
     return full_dum_data, pd.DataFrame(price)
+
+
+def ord_enc_func(housing):
+    housing = housing.reset_index() #duplicated index values in csv need to reset
+    housing = housing.drop('index', axis = 1) # drop original index with duplicates
+    price = housing['SalePrice'] # Create Y Variable
+    category = housing.select_dtypes('object') #Select all 'object' data types  which are all categorical
+    housing_num = housing.select_dtypes(['int64', 'float64']) # Select numeric data types
+    housing_num_PID = housing_num['PID'] # PID index should not be scaled, remove and put back later
+    ## Numeric Colums to convert
+    # MSSubClass, OverallQual, OverallCond, YearBuilt, YearRemodAdd, MoSold, YrSold
+    # How to handle MiscVal???
+    #Leave YearBuilt and YearRemodAdd as numeric to be scaled
+    housing_num = housing_num.drop(['PID', 'SalePrice', 'MSSubClass', 'OverallQual', \
+    'OverallCond', 'MoSold', 'YrSold', 'MiscVal'], axis = 1)
+    housing_num2cat = housing[['MSSubClass', 'OverallQual', 'OverallCond', \
+    'MoSold', 'YrSold', 'MiscVal']]
+    category = pd.concat([category.astype(str), housing_num2cat.astype(str)], axis = 1) #Add all categorical features to dataframe to be dummified
+    oe = OrdinalEncoder()
+    cat_ord_enc = oe.fit_transform(category)
+    scaler = MinMaxScaler()
+    scaler.fit(housing_num)
+    housing_num_scaled = scaler.transform(housing_num)
+    housing_num_scaled = pd.DataFrame(housing_num_scaled, columns = housing_num.columns)
+    cat_ord_enc =pd.DataFrame(cat_ord_enc, columns = category.columns)
+    full_oe_data = pd.concat([housing_num_PID, housing_num_scaled, cat_ord_enc], axis = 1) #Concatenate dummified data and numeric data
+    return full_oe_data, price
