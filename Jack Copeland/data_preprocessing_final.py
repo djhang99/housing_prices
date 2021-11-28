6910e30feb618deb1c6366a0111d401ae747e1f5
Jack Copeland <jackcopeland11@gmail.com>
2021-11-28 10:39:10 -0500

    Changes to preprocessing file

--- Jack Copeland/data_preprocessing_final.py
+++ Jack Copeland/data_preprocessing_final.py
@@ -61,7 +61,7 @@ def cleaning(dataframe):
 
     ## Add a total baths feature and remove the original columns
     housing['TotalBath'] = housing['FullBath'] + (housing['HalfBath']*0.5) + housing['BsmtFullBath'] + (housing['BsmtHalfBath']*0.5)
-    baths_drop = ['HalfBath', 'FullBath', 'BsmtFullBath', 'BsmtHalfBath', 'TotalBath']
+    baths_drop = ['HalfBath', 'FullBath', 'BsmtFullBath', 'BsmtHalfBath']
     housing.drop(columns= baths_drop, inplace=True, axis =1)
 
     ## Add ratio for unfinished basement space -- Make sure this is always before "TotalLivArea" is created or columns will be dropped
@@ -117,6 +117,8 @@ def cleaning(dataframe):
 
     housing = housing[~housing.PID.isin([904300150, 535383070, 905426030, 528142130])]
 
+    housing = housing[housing['SaleCondition'] == 'Normal']
+
     return housing
 
 
@@ -155,7 +157,7 @@ def scale_data(dataframe, scaler):
     dataframe_num = dataframe[['LotFrontage', 'LotArea', 'YearBuilt', 'YearRemodAdd', 'MasVnrArea',
                             'BedroomAbvGr', 'TotRmsAbvGrd', 'Fireplaces', 'GarageYrBlt',
                             'GarageArea', 'WoodDeckSF', 'OpenPorchSF', 'EnclosedPorch', '3SsnPorch',
-                            'ScreenPorch', 'Bsmt_Unfin_Ratio', 'TotalLivArea']] # Select columns that were used to train the scaler
+                            'ScreenPorch', 'Bsmt_Unfin_Ratio', 'TotalLivArea','TotalBath' ]] # Select columns that were used to train the scaler
 
     ## Drop the original columns from the main dataframe 
     dataframe.drop(columns = dataframe_num.columns, axis = 1, inplace =True)
